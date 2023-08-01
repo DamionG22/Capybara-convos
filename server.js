fc3b9dbea8c6a084afc24ae308a056edb367011b
User <lexin_wiesner@yahoo.com>
2023-07-31 19:28:40 -0700

    deleted html file and added handlebars as our main view

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-var express = require('express')
-var app = express()
-var socket = require('./socket')
-var http = require('http').createServer(app)
-var io = require('socket.io')(http)
+var express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
-io.attach(http)
+var app = express();
+const PORT = process.env.PORT || 3001;
+
+var socket = require('./socket');
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+io.attach(http);
 socket(io);
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-})
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-http.listen(3001, function () {
+app.use(express.static('public'));
+
+http.listen(PORT, () => {
     console.log('listening on 3001');
 })
 
