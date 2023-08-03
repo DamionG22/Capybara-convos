e5d33772d816451c6b0153d73520c503a38ae425
User <lexin_wiesner@yahoo.com>
2023-08-02 20:56:58 -0700

    Updated some files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
@@ -20,6 +20,24 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('homepage', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
