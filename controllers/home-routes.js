4f89026bed5fb709c540af52792db750fb6ac768
User <lexin_wiesner@yahoo.com>
2023-08-02 21:25:30 -0700

    Updated routes with proper paths

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
@@ -26,7 +26,6 @@ router.get('/login', (req, res) => {
     res.redirect('/');
     return;
   }
-
   res.render('login');
 });
 
